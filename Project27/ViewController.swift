71d3bf35a716e0e31fc445d18189ecc952dfe766
Kirsty Darbyshire <kirsty@loquax.com>
2019-05-06 21:03:07 +0100

    Initial Commit

---
+++ Project27/ViewController.swift
@@ -0,0 +1,251 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by kirsty darbyshire on 06/05/2019.
+//  Copyright © 2019 nocto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
+    
+    
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 7 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        case 1:
+            drawCircle()
+        case 2:
+            drawCheckerboard()
+        case 3:
+            drawRotatedSquares()
+        case 4:
+            drawLines()
+        case 5:
+            drawImagesAndText()
+        case 6:
+            drawStar()
+        case 7:
+            drawTWIN()
+            
+        default: break
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        drawRectangle()
+    }
+
+    func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
+            
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawCheckerboard() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.black.cgColor)
+            
+            for row in 0..<8 {
+                for col in 0..<8 {
+                    if (row + col) % 2 == 0 {
+                        ctx.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawRotatedSquares() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            ctx.cgContext.translateBy(x: 256, y: 256)
+            
+            let rotations = 16
+            let amount = Double.pi / Double(rotations)
+            
+            for _ in 0 ..< rotations {
+                ctx.cgContext.rotate(by: CGFloat(amount))
+                ctx.cgContext.addRect(CGRect(x: -128, y: -128, width: 256, height: 256))
+            }
+            
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawLines() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            ctx.cgContext.translateBy(x: 256, y: 256)
+            
+            var first = true
+            var length: CGFloat = 256
+            
+            for _ in 0 ..< 256 {
+                ctx.cgContext.rotate(by: .pi / 2)
+                if first {
+                    ctx.cgContext.move(to: CGPoint(x: length, y: 50))
+                    first = false
+                } else {
+                    ctx.cgContext.addLine(to: CGPoint(x: length, y: 50))
+                }
+                length *= 0.99
+            }
+            
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawImagesAndText() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 36),
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            let string = "The best laid schemes o'\nmice an' men gang aft agley"
+            
+            let attributedString = NSAttributedString(string: string, attributes: attrs)
+            attributedString.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin, context: nil)
+            
+            let mouse = UIImage(named: "mouse")
+            mouse?.draw(at: CGPoint(x: 300, y: 150))
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawStar() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            ctx.cgContext.translateBy(x: 256, y: 256)
+            
+            let length: CGFloat = 256
+            
+            ctx.cgContext.rotate(by:  .pi / 10)
+            ctx.cgContext.move(to: CGPoint(x: 256, y: 0))
+
+            for _ in 0 ..< 5 {
+                ctx.cgContext.addLine(to: CGPoint(x: length, y: 0))
+                ctx.cgContext.rotate(by: 4 * .pi / 5)
+            }
+            
+            ctx.cgContext.setFillColor(UIColor.yellow.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.yellow.cgColor)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawTWIN() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+
+            /// T
+            gridMove(x: 0, y: 0, ctx: ctx)
+            gridLine(x: 2, y: 0, ctx: ctx)
+            gridMove(x: 1, y: 0, ctx: ctx)
+            gridLine(x: 1, y: 2, ctx: ctx)
+            
+            /// W
+            gridMove(x: 3, y: 0, ctx: ctx)
+            gridLine(x: 4, y: 2, ctx: ctx)
+            gridLine(x: 4.5, y: 1, ctx: ctx)
+            gridLine(x: 5, y: 2, ctx: ctx)
+            gridLine(x: 6, y: 0, ctx: ctx)
+
+            /// I
+            gridMove(x: 7, y: 0, ctx: ctx)
+            gridLine(x: 7, y: 2, ctx: ctx)
+            
+            /// N
+            gridMove(x: 8, y: 2, ctx: ctx)
+            gridLine(x: 8, y: 0, ctx: ctx)
+            gridLine(x: 10, y: 2, ctx: ctx)
+            gridLine(x: 10, y: 0, ctx: ctx)
+            
+            
+            ctx.cgContext.setLineWidth(2)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.strokePath()
+        }
+        
+        imageView.image = image
+    }
+    
+    func gridMove(x: Double, y: Double, ctx: UIGraphicsImageRendererContext) {
+        let length: Double = 512/12
+        let offset: Double = 50
+        ctx.cgContext.move(to: CGPoint(x: x * length + offset, y: y * length + offset))
+    }
+    
+    func gridLine(x: Double, y: Double, ctx: UIGraphicsImageRendererContext) {
+        let length: Double = 512/12
+        let offset: Double = 50
+        ctx.cgContext.addLine(to: CGPoint(x: x * length + offset, y: y * length + offset))
+    }
+
+
+}
+
